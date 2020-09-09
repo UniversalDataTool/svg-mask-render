be9d9413cfa284cf7b1b3f912d0165bfe301fd4e
seveibar <seveibar@gmail.com>
2020-09-09 16:02:26 -0400

    remove any shared library dependencies

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > This library is in beta, but being used by the Universal Data Tool. Contributions are appreciated!
 
-Render SVGs on node and web. Uses [skia-canvas](https://github.com/samizdatco/skia-canvas) and [canvg](https://github.com/canvg/canvg).
+Render SVGs on node and web. Uses [node-pureimage](https://github.com/joshmarinacci/node-pureimage) and [canvg](https://github.com/canvg/canvg).
 
 Existing libraries often had problems with serverless platforms, inconsistency across machines, forced anti-aliasing or unnecessary complexity.
 
@@ -14,21 +14,8 @@ const render = require("svg-render")
 render(svgText, { output: "pngbuffer" }).then((pngBuffer) => {
   // fs.writeFile("./myImage.png", pngBuffer)
 })
-
-
-// Want to eliminate anti-aliasing? No problem, provide allowed colors
-// and every color will be rounded to the nearest colors
-render(svgText, { output: "pngbuffer", allowedColors:[
-  [255,255,255,255], // white
-  [255,0,0,255], // red
-]})
-  .then((pngBuffer) => {
-    // fs.writeFile("./myImage.png", pngBuffer)
-  })
-
 ```
 
 ## TODO
 
 - [] Font Support
-- [] Support allowedColors with string colors
--- index.js
+++ index.js
@@ -1,47 +1,8 @@
 const jsdom = require("jsdom")
 const canvg = require("canvg")
-const { Canvas, Image } = require("skia-canvas")
-
-function colorDistance(c1, c2) {
-  // Manhattan is a dumb distance metric, but at the moment it's unclear
-  // to me which color distance metric makes the most sense for anti-aliased
-  // effects on images
-  return (
-    Math.abs(c1[0] - c2[0]) +
-    Math.abs(c1[1] - c2[1]) +
-    Math.abs(c1[2] - c2[2]) +
-    Math.abs(c1[3] - c2[3])
-  )
-}
-
-function modifyContextAndRoundColors(ctx, colors, { width, height }) {
-  const imData = ctx.getImageData(0, 0, width, height)
-  const pixels = imData.data
-  for (let i = 0; i < pixels.length; i += 4) {
-    const rgba = pixels.slice(i, i + 4)
-    let lowestD = Infinity
-    let lowestU = 0
-    let needsRounding = true
-    for (let u = 0; u < colors.length; u++) {
-      const d = colorDistance(rgba, colors[u])
-      if (d === 0) {
-        needsRounding = false
-        break
-      } else if (d < lowestD) {
-        lowestD = d
-        lowestU = u
-      }
-    }
-    if (needsRounding) {
-      pixels[i] = colors[lowestU][0]
-      pixels[i + 1] = colors[lowestU][1]
-      pixels[i + 2] = colors[lowestU][2]
-      pixels[i + 3] = colors[lowestU][3]
-    }
-  }
-  ctx.clearRect(0, 0, width, height)
-  ctx.putImageData(imData, 0, 0)
-}
+const stream = require("stream")
+const PImage = require("pureimage")
+const streams = require("memory-streams")
 
 module.exports = async (
   svgText,
@@ -63,29 +24,29 @@ module.exports = async (
     throw new Error("svg width and height must be provided")
   }
 
-  const canvas = new Canvas(width, height)
+  w.createCanvas = PImage.make
+
+  const canvas = PImage.make(width, height)
+  canvas.width = width
+  canvas.height = height
   const ctx = canvas.getContext("2d")
   ctx.imageSmoothingEnabled = false
+  ctx.canvas = canvas
 
   const svgRenderer = canvg.Canvg.fromString(ctx, svgText, {
     DOMParser: w.DOMParser,
     window: w,
     ignoreMouse: true,
     ignoreAnimation: true,
-    ImageClass: Image,
+    canvas: canvas,
+    createCanvas: PImage.make,
   })
 
   svgRenderer.render()
 
-  if (options.allowedColors) {
-    // Prevent anti-aliasing by interating over each pixel and rounding
-    // it to the nearest color
-    modifyContextAndRoundColors(ctx, options.allowedColors, { width, height })
-  }
+  const outputStream = new streams.WritableStream()
 
-  const dataUrl = canvas.toDataURL("image/png")
+  await PImage.encodePNGToStream(canvas, outputStream)
 
-  const pngInBase64 = dataUrl.slice("data:image/png;base64,".length)
-
-  return Buffer.from(pngInBase64, "base64")
+  return outputStream.toBuffer()
 }
